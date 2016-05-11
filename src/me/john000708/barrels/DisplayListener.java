2c3b930b7b74b3866a8f8d3521d4ecb1eb0b3cce
mrCookieSlime <mrCookieSlime@gmail.com>
2016-05-11 22:32:38 +0200

    Fixed NPE

--- src/me/john000708/barrels/DisplayListener.java
+++ src/me/john000708/barrels/DisplayListener.java
@@ -16,14 +16,14 @@ public class DisplayListener implements Listener {
 
     @EventHandler
     public void onPickpup(PlayerPickupItemEvent e) {
-        if (!e.getItem().hasMetadata("no_pickup") && e.getItem().getItemStack().hasItemMeta() && e.getItem().getItemStack().getItemMeta().getDisplayName().startsWith("§6§lB4R3L - §eITEM")) {
+        if (!e.getItem().hasMetadata("no_pickup") && e.getItem().getItemStack().hasItemMeta() && e.getItem().getItemStack().getItemMeta().hasDisplayName() && e.getItem().getItemStack().getItemMeta().getDisplayName().startsWith("§6§lB4R3L - §eITEM")) {
         	e.getItem().remove();
         }
     }
 
     @EventHandler
     public void onInventoryPickup(InventoryPickupItemEvent e) {
-        if (!e.getItem().hasMetadata("no_pickup") && e.getItem().getItemStack().hasItemMeta() && e.getItem().getItemStack().getItemMeta().getDisplayName().startsWith("§6§lB4R3L - §eITEM")) {
+        if (!e.getItem().hasMetadata("no_pickup") && e.getItem().getItemStack().hasItemMeta() && e.getItem().getItemStack().getItemMeta().hasDisplayName() && e.getItem().getItemStack().getItemMeta().getDisplayName().startsWith("§6§lB4R3L - §eITEM")) {
             e.getItem().remove();
         }
     }
