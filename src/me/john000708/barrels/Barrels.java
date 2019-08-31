4f7a23b81c659a8522a6f3f1491a6652c5e41918
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2019-08-31 15:19:12 +0300

    Undo-ing import changes for backwards compability.

--- src/me/john000708/barrels/Barrels.java
+++ src/me/john000708/barrels/Barrels.java
@@ -24,7 +24,7 @@ import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
-import me.mrCookieSlime.Slimefun.Objects.handlers.ItemInteractionHandler;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.handlers.ItemInteractionHandler;
 import me.mrCookieSlime.Slimefun.Setup.SlimefunManager;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 
