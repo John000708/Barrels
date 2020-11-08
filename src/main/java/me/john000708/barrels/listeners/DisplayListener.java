92c5161d1e1bfffbbe0b7a7f997c560ec4489d69
bito-blosh <53908465+bito-blosh@users.noreply.github.com>
2020-11-08 13:26:01 +0900

    Update DisplayListener.java

--- src/main/java/me/john000708/barrels/listeners/DisplayListener.java
+++ src/main/java/me/john000708/barrels/listeners/DisplayListener.java
@@ -3,12 +3,13 @@ package me.john000708.barrels.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.ItemMergeEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
 
 import me.john000708.barrels.Barrels;
 import me.john000708.barrels.DisplayItem;
-import me.mrCookieSlime.Slimefun.cscorelib2.data.PersistentDataAPI;
 
 /**
  * Created by John on 10.05.2016.
@@ -20,18 +21,46 @@ public class DisplayListener implements Listener {
     }
 
     @EventHandler
-    public void onPickpup(EntityPickupItemEvent e) {
-        if (!e.getItem().hasMetadata("no_pickup") && PersistentDataAPI.hasString(e.getItem(), DisplayItem.NAMESPACED_KEY)) {
+    public void onEntityDamage(EntityDamageEvent e) {
+        if (DisplayItem.isDisplayItem(e.getEntity())) {
             e.setCancelled(true);
-            e.getItem().remove();
+            if (!DisplayItem.isCurrentLocation(e.getEntity())) {
+                DisplayItem.removeDisplayItem(e.getEntity());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent e) {
+        if (DisplayItem.isDisplayItem(e.getItem())) {
+            e.setCancelled(true);
+            if (DisplayItem.isCurrentLocation(e.getItem())) {
+                e.getItem().setPickupDelay(20);
+            }
+            else {
+                DisplayItem.removeDisplayItem(e.getItem());
+            }
         }
     }
 
     @EventHandler
     public void onInventoryPickup(InventoryPickupItemEvent e) {
-        if (!e.getItem().hasMetadata("no_pickup") && PersistentDataAPI.hasString(e.getItem(), DisplayItem.NAMESPACED_KEY)) {
+        if (DisplayItem.isDisplayItem(e.getItem())) {
             e.setCancelled(true);
-            e.getItem().remove();
+            if (DisplayItem.isCurrentLocation(e.getItem())) {
+                e.getItem().setPickupDelay(20);
+            }
+            else {
+                DisplayItem.removeDisplayItem(e.getItem());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onItemMerge(ItemMergeEvent e) {
+        if (DisplayItem.isDisplayItem(e.getEntity())) {
+            e.setCancelled(true);
+            e.getTarget().remove();
         }
     }
 }
